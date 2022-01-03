9989e27e3db7120a7f6c28b52a038d571e87fdb4
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-01-03 17:35:53 +0300

    Added an upvote button on the quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,10 +10,10 @@ title = 'Motivational quotes';
 quotes: Quote[] = [
     new Quote(1,'Nancy','Dream Big','When you have a dream,', 'you’ve got to grab it and never let go.',new Date(2020,3,14),0,0),
     new Quote(2,'Meyer','About LIfe','Life has got all those twists and turns.','You’ve got to hold on tight and off you go',new Date(2019,6,9),0,0),
-    new Quote(3,'Victoria','Shine your Face','Keep your face always toward the sunshine','and shadows will fall behind you',new Date(2022,1,12),0,0),
+    new Quote(3,'Victoria','Shine your Face','Keep your face always toward the sunshine','and shadows will fall behind you',new Date(2021,1,12),0,0),
     new Quote(4,'Stephen','Success','Success is not final, failure is not fatal','it is the courage to continue that counts.',new Date(2019,0,18),0,0),
     new Quote(5,'Eric','Define your life','You define your own life','Don’t let other people write your script.',new Date(2019,2,14),0,0),
-    new Quote(6,'Ann','All you want','You can be everything','You can be the infinite amount of things that people are.',new Date(2030,3,14),0,0),
+    new Quote(6,'Ann','All you want','You can be everything','You can be the infinite amount of things that people are.',new Date(2020,3,14),0,0),
   ];
   get sortQuotes() {
     return this.quotes.sort((a, b) => {
